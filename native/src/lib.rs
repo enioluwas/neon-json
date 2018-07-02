806dd2c035b3fab7ab16105c5cf029b9d77312e7
Enioluwa Segun <eos24@drexel.edu>
2018-07-01 20:22:41 -0400

    Added reviver functionality to parse(). Updated tests
    Code style changes too.

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,36 +4,74 @@ extern crate neon_serde;
 extern crate serde_json;
 
 use neon::js::error::{JsError, Kind};
-use neon::js::{JsString, JsValue};
+use neon::js::{Object, JsString, JsValue, JsFunction, JsObject, JsNull};
 use neon::vm::{Call, JsResult};
+use neon::mem::Handle;
+use neon::scope::RootScope;
 
-fn parse(call: Call) -> JsResult<JsValue>
+fn parse(call: Call) -> JsResult<JsObject>
 {
     let scope = call.scope;
-    let string = call.arguments
-        .require(scope, 0)?
-        .check::<JsString>()?;
+    let args_len = call.arguments.len();
 
-    let object: serde_json::Value = serde_json::from_str(&string.value())
-        .or_else(|err| JsError::throw(Kind::Error, &err.to_string()))?;
+    let js_string = call.arguments.require(scope, 0)?.check::<JsString>()?;
 
-    let object = neon_serde::to_value(scope, &object)?;
+    let js_object: serde_json::Value = serde_json::from_str(&js_string.value())
+        .or_else(|err| {
+            JsError::throw(Kind::Error, &err.to_string())
+        })?;
 
-    Ok(object)
+    let js_object = neon_serde::to_value(scope, &js_object)?.check::<JsObject>()?;
+
+    let _obj = js_object;
+
+    if args_len > 1
+        {
+            let js_reviver = call.arguments
+                .require(scope, 1)?
+                .check::<JsFunction>()?;
+
+            let _obj = call_reviver(scope, js_object, js_reviver)?;
+        }
+
+    Ok(_obj)
+}
+
+fn call_reviver<'a>(scope: &'a mut RootScope, obj: Handle<'a, JsObject>, reviver: Handle<'a, JsFunction>) -> JsResult<'a, JsObject>
+{
+    let obj_keys = obj.get_own_property_names(scope)?
+        .to_vec(scope)?;
+
+    for key in obj_keys
+        {
+            let old_val = obj.get(scope, key).unwrap();
+            if old_val.is_a::<JsObject>()
+            {
+                obj.set(key, call_reviver(scope, old_val.check::<JsObject>()?, reviver)?)?;
+            }
+            else
+            {
+                let args = vec![key, old_val];
+                let new_val = reviver.call(scope, JsNull::new(), args.into_iter())?;
+                obj.set(key, new_val)?;
+            }
+        }
+
+    Ok(obj)
 }
 
 fn stringify(call: Call) -> JsResult<JsString>
 {
     let scope = call.scope;
-    let object = call.arguments
-        .require(scope, 0)?
-        .check::<JsValue>()?;
+    let js_object = call.arguments.require(scope, 0)?.check::<JsValue>()?;
 
-    let object: serde_json::Value = neon_serde::from_value(scope, object)?;
-    let string = serde_json::to_string(&object)
-        .or_else(|err| JsError::throw(Kind::Error, &err.to_string()))?;
+    let js_object: serde_json::Value = neon_serde::from_value(scope, js_object)?;
+    let js_string = serde_json::to_string(&js_object)
+        .or_else(|err| {
+            JsError::throw(Kind::Error, &err.to_string())
+        })?;
 
-    JsString::new_or_throw(scope, &string)
+    JsString::new_or_throw(scope, &js_string)
 }
 
 register_module!(m, 
--- test/index.js
+++ test/index.js
@@ -5,16 +5,36 @@ const util = require('util')
 
 const MAX_TIME = 5000
 
-const confirmP = async (neonParsed, input) =>
+const confirmP = async (neonParsed, input, reviver) =>
 {
-    const jsonParsed = JSON.parse(input)
-    expect(neonParsed).to.deep.equal(jsonParsed)
+    if(typeof reviver === "undefined")
+    {
+        const jsonParsed = JSON.parse(input);
+        expect(neonParsed).to.deep.equal(jsonParsed);
+    }
+
+    else
+    {
+        const jsonParsed = JSON.parse(input, reviver);
+        expect(neonParsed).to.deep.equal(jsonParsed);
+    }
 }
 
 const confirmS = async (neonStringified, input) =>
 {
-    const jsonStringified = JSON.stringify(input)
-    expect(jsonStringified === neonStringified)
+    const jsonStringified = JSON.stringify(input);
+    expect(jsonStringified === neonStringified);
+}
+
+const dataTransfomer = function(key, value)
+{
+    if(key === "_id")
+    {
+        var newId = "001";
+        return newId; 
+    }
+
+    return value;
 }
 
 const readFile = util.promisify(fs.readFile)
@@ -23,7 +43,7 @@ const getString = async function()
 {
     await readFile("test/example.json", "utf8")
         .then(function(data){
-            input = data.slice(0)
+            input = data.slice(0);
         }.bind(this))
 }
 
@@ -31,7 +51,7 @@ const getJson = async function()
 {
     await readFile("test/example.json", "utf8")
         .then(function(data){
-            input = JSON.parse(data)
+            input = JSON.parse(data);
         }.bind(this),
         function(){
             console.error("Failed to read file"); 
@@ -40,19 +60,29 @@ const getJson = async function()
 
 describe("Neon-JSON tests", function()
 {
-    this.timeout(MAX_TIME)
+    this.timeout(MAX_TIME);
 
     it("Parse String to JSON object", async()=>
     {
-       await getString()
-       const neonParsed = NeonJSON.parse(input)
-       await confirmP(neonParsed, input)
+       await getString();
+       const neonParsed = NeonJSON.parse(input);
+       await confirmP(neonParsed, input);
+    })
+
+    it("Apply Parse Reviver on JSON object", async()=>
+    {
+        await getString();
+        const neonParsed = NeonJSON.parse(input, dataTransfomer);
+        await confirmP(neonParsed, input, dataTransfomer);
     })
 
     it("Stringify JSON Object", async()=>
     {
-        await getJson()
-        const neonStringified = NeonJSON.stringify(input)
-        await confirmS(neonStringified, input)
+        await getJson();
+        const neonStringified = NeonJSON.stringify(input);
+        await confirmS(neonStringified, input);
     })
+
+    //TODO
+    it("Apply Stringify Replacer on String")
 })
